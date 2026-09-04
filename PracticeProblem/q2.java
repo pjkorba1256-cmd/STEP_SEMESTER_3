8f6dd113165e7283c4c77c928ffabab8e2788b8f
pjkorba1256-cmd <pj.korba1256@gmail.com>
2026-09-04 13:28:14 +0530

    Implement palindrome checker in q5 class
    Added a new class q5 with a palindrome checker and user input.

--- PracticeProblem/q2.java
+++ PracticeProblem/q2.java
@@ -1,5 +1,38 @@
-package PracticeProblem;
+import java.util.Scanner;
 
-public class q2 {
-    
+public class q5 {
+
+    static boolean isPalindrome(String text) {
+
+        int start = 0;
+        int end = text.length() - 1;
+
+        while (start < end) {
+
+            if (text.charAt(start) != text.charAt(end)) {
+                return false;
+            }
+
+            start++;
+            end--;
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a word: ");
+        String text = sc.next();
+
+        if (isPalindrome(text)) {
+            System.out.println("Palindrome");
+        } else {
+            System.out.println("Not Palindrome");
+        }
+
+        sc.close();
+    }
 }
