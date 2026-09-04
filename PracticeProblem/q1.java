b1e31a7312e0b004b63ba4f6e9d0f349fbe261a2
pjkorba1256-cmd <pj.korba1256@gmail.com>
2026-09-04 13:23:45 +0530

    Implement Rock-Paper-Scissors game logic

--- PracticeProblem/q1.java
+++ PracticeProblem/q1.java
@@ -1,5 +1,79 @@
-package PracticeProblem;
+import java.util.*;
 
-public class q1 {
-    
+public class RockPaperScissors {
+
+    static String playRound(String playerMove, String computerMove) {
+
+        if (playerMove.equals(computerMove)) {
+            return "Draw";
+        }
+
+        if ((playerMove.equals("Rock") && computerMove.equals("Scissors")) ||
+            (playerMove.equals("Paper") && computerMove.equals("Rock")) ||
+            (playerMove.equals("Scissors") && computerMove.equals("Paper"))) {
+            return "Player Wins";
+        }
+
+        return "Computer Wins";
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        Random random = new Random();
+
+        String[] moves = {"Rock", "Paper", "Scissors"};
+
+        int wins = 0;
+        int losses = 0;
+        int draws = 0;
+
+        String[] playerMoves = new String[5];
+        String[] computerMoves = new String[5];
+        String[] results = new String[5];
+
+        for (int i = 0; i < 5; i++) {
+
+            System.out.print("Enter Rock, Paper or Scissors: ");
+            String playerMove = sc.next();
+
+            String computerMove = moves[random.nextInt(3)];
+
+            String result = playRound(playerMove, computerMove);
+
+            playerMoves[i] = playerMove;
+            computerMoves[i] = computerMove;
+            results[i] = result;
+
+            System.out.println("Computer: " + computerMove);
+            System.out.println("Result: " + result);
+            System.out.println();
+
+            if (result.equals("Player Wins"))
+                wins++;
+            else if (result.equals("Computer Wins"))
+                losses++;
+            else
+                draws++;
+        }
+
+        System.out.println("----- FINAL SUMMARY -----");
+        System.out.println("Round\tPlayer\tComputer\tResult");
+
+        for (int i = 0; i < 5; i++) {
+            System.out.println((i + 1) + "\t" +
+                    playerMoves[i] + "\t" +
+                    computerMoves[i] + "\t" +
+                    results[i]);
+        }
+
+        double winPercentage = (wins * 100.0) / 5;
+
+        System.out.println("\nWins: " + wins);
+        System.out.println("Losses: " + losses);
+        System.out.println("Draws: " + draws);
+        System.out.println("Win %: " + winPercentage + "%");
+
+        sc.close();
+    }
 }
